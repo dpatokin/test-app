8a84835c7fae1c7cb47428d7531658b57bc8248a
Daniil Patokin <dpatokin@devexperts.com>
2025-04-26 18:50:36 +0300

    MVP app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button as MUIButton } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { useState } from "react";
+
+interface Series {
+  url: string;
+  name: string;
+  image?: { medium: string; original: string };
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seriesData, setSeriesData] = useState([]);
+
+  const fetchData: () => void = async () => {
+    try {
+      const pageNumber: number = Math.floor(Math.random() * 336);
+      console.log(pageNumber);
+      const response = await fetch(
+        `https://api.tvmaze.com/shows?page=${pageNumber}`,
+      );
+      const result = await response.json();
+
+      setSeriesData(result);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    } finally {
+      // setLoading(false);
+    }
+  };
+  const seriesList =
+    seriesData.length > 0 &&
+    seriesData.map((series: Series, i) => (
+      <Grid size={2} className="seriesCard" key={i}>
+        <a href={series.url} target="_blank" rel="noopener noreferrer">
+          <h3 key={i}>{series.name}</h3>
+          {series.image && <img src={series.image.medium} alt={series.name} />}
+        </a>
+      </Grid>
+    ));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MUIButton variant="contained" onClick={fetchData}>
+        Get series
+      </MUIButton>
+      {seriesData.length > 0 && (
+        <Grid container spacing={3} mt={8}>
+          {seriesList}
+        </Grid>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
