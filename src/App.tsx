8311c91be7ef17625dfa4f999078554a4e608b64
Daniil Patokin <dpatokin@devexperts.com>
2025-05-02 11:39:06 +0300

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorite from "./pages/Favorite";
-import { Container } from "@mui/material";
+import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import TheatersIcon from "@mui/icons-material/Theaters";
 
 function App() {
   return (
-    <Container sx={{ py: 8 }}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/favorite">Favorite</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorite" element={<Favorite />} />
-      </Routes>
-    </Container>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <TheatersIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Cinema Finder
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/favorite">
+            Favorite
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ pt: 17, pb: 8 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
