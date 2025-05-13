2cbc72d135b95c9b88e42b5b0970ef38d2c2e9a0
Daniil Patokin <dpatokin@devexperts.com>
2025-05-13 17:23:21 +0300

    Added prefetch random page number

--- src/hooks/useFetchMedia.ts
+++ src/hooks/useFetchMedia.ts
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { MediaItem, MediaSortType } from "../types";
 
-// TODO: move function to separated type
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+
+// TODO: move function to separated type, refactor amount of params (only need 2 of them)
+// TODO: prefetch pages only if mediaSortType is changed after prev fetch
 export default function useFetchMedia(): {
   fetchMedia: (
     mediaSortType: MediaSortType,
@@ -13,6 +17,27 @@ export default function useFetchMedia(): {
   mediaData: MediaItem[];
 } {
   const [data, setData] = useState<MediaItem[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+
+  async function fetchTotalPages(url: string): Promise<number> {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        console.error(`HTTP error! Status: ${response.status}`);
+        return 0;
+      }
+      const result = await response.json();
+
+      setTotalPages(result.total_pages);
+
+      return result.total_pages;
+    } catch (error) {
+      console.error("Error fetching total pages:", error);
+      return 0;
+    }
+  }
+
   const fetchMedia = async (
     mediaSortType: MediaSortType,
     movieName: string,
@@ -20,13 +45,30 @@ export default function useFetchMedia(): {
     genre: string,
     language: string,
   ): Promise<void> => {
-    const url = getURL(mediaSortType, movieName, year, genre, language);
+    let url = getURL(mediaSortType, movieName, year, genre, language);
+
+    if (mediaSortType !== "name") {
+      let total = totalPages || (await fetchTotalPages(url));
+      total = total > 500 ? 500 : total; // There is an API bug if you try to set page number more than 500
+
+      if (total > 0) {
+        const pageNumber = Math.floor(Math.random() * total) + 1;
+
+        url += `&page=${pageNumber}`;
+      }
+    }
 
     try {
       const response: Response = await fetch(url);
+
+      if (!response.ok) {
+        console.error(`HTTP error! Status: ${response.status}`);
+        return;
+      }
+
       const result = await response.json();
 
-      setData(result.results);
+      setData(result.results || []);
     } catch (error) {
       console.error("Fetch error:", error);
     }
@@ -42,14 +84,11 @@ function getURL(
   genre: string,
   language: string,
 ): string {
-  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-  const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
   let url: string = "";
 
   switch (mediaSortType) {
     case "random": {
-      const pageNumber = Math.floor(Math.random() * 501);
-      url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${pageNumber}`;
+      url = `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
       break;
     }
     case "name":
