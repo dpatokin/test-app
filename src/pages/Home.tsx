708c27e5398f60e66fc6b02dbcf8b1f3a4747e46
Daniil Patokin <dpatokin@devexperts.com>
2025-05-25 16:16:47 +0300

    Check if API is available

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
-import { Container } from "@mui/material";
+import { Box, CircularProgress, Container, Typography } from "@mui/material";
 import CardsList from "../components/CardsList.tsx";
 import useFetchMedia from "../hooks/useFetchMedia.ts";
 import { Form } from "../components/Form/Form.tsx";
 import { useState } from "react";
 import { FetchMediaParams } from "../types";
+import useAPIStatus from "../hooks/useAPIStatus.ts";
 
 export default function Home() {
   const { fetchMedia, mediaData } = useFetchMedia();
   const [loading, setLoading] = useState(false);
+  const { isAPIWorking, loading: apiLoading } = useAPIStatus();
 
   const handleFetchMedia = async (params: FetchMediaParams) => {
     setLoading(true);
@@ -15,10 +17,32 @@ export default function Home() {
     setLoading(false);
   };
 
+  let content;
+
+  if (apiLoading) {
+    content = (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
+  } else if (!isAPIWorking) {
+    content = (
+      <Typography variant="h2" align="center">
+        The service is currently unavailable
+      </Typography>
+    );
+  } else {
+    content = (
+      <>
+        <Form fetchMedia={handleFetchMedia} loading={loading} />
+        <CardsList mediaData={mediaData} loading={loading} />
+      </>
+    );
+  }
+
   return (
     <Container maxWidth="xl" sx={{ pt: 17, pb: 8 }}>
-      <Form fetchMedia={handleFetchMedia} loading={loading} />
-      <CardsList mediaData={mediaData} loading={loading} />
+      {content}
     </Container>
   );
 }
--- todo.md
+++ todo.md
@@ -1,7 +1,6 @@
 # TODO
 
 * [ ]  Add pagination
-* [ ]  Check on App load if api is working
 * [ ]  Check useMemo
 * [ ]  Move localstorage to DB
 * [ ]  Add bottom nav for mobile version - https://mui.com/material-ui/react-bottom-navigation/
